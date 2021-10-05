dbe65b9e2d43ce6331e03845ae86091f2135b516
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-05 14:05:43 +0000

    problem

---
+++ index.js
@@ -0,0 +1,2 @@
+/* Write a recursive function called isPalindrome which return strue if the string passed to it is a palindrome 
+(reads the same forward and backward). Otherwise it returns false. */
